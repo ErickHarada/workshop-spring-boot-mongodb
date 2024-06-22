d56dfb227c0e2b3c105b04e89af658ea875c31ee
Erick <eefeaga2000@gmail.com>
2024-06-22 13:06:26 -0300

    Obtendo um usuário por id

--- src/main/java/com/harada/workshopmongo/resources/UserResource.java
+++ src/main/java/com/harada/workshopmongo/resources/UserResource.java
@@ -5,9 +5,7 @@ import com.harada.workshopmongo.dto.UserDTO;
 import com.harada.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,11 @@ public class UserResource {
         List<User> list = userService.findAll();
         List<UserDTO> listDto = list.stream().map(UserDTO::new).toList();
         return ResponseEntity.ok(listDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok(new UserDTO(user));
     }
 }
