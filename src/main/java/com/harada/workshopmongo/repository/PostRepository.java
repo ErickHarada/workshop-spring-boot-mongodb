5a907c8d5ff337a15a283584461e4c0c706d980d
Erick <eefeaga2000@gmail.com>
2024-07-16 18:46:54 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/harada/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/harada/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,9 @@
 package com.harada.workshopmongo.repository;
 
-import com.harada.workshopmongo.domain.User;
+import com.harada.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 }
