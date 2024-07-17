ec62a4ab2dd0e595e79464a60bd603d2383e1554
Erick <eefeaga2000@gmail.com>
2024-07-17 19:17:27 -0300

    Consulta com varios criterios

--- src/main/java/com/harada/workshopmongo/resources/PostResource.java
+++ src/main/java/com/harada/workshopmongo/resources/PostResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -37,6 +38,20 @@ public class PostResource {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(defaultValue = "") String text) {
         text = URL.decodeParam(text);
         List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok(list);
+    }
+
+
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(defaultValue = "") String text,
+            @RequestParam(defaultValue = "") String minDate,
+            @RequestParam(defaultValue = "") String maxDate
+    ) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text, min, max);
         return ResponseEntity.ok(list);
     }
 }
